ce637f0e271990b04f101ac5d25823a046a6c958
Saurav <kumarsaurav26198@gmail.com>
2022-03-30 19:28:44 +0530

    Adding get method

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -1,3 +1,16 @@
 const mongoose = require('mongoose');// Using Node.js `require()`
-mongoose.connect("mongodb://127.0.0.1:27017/form").then(() => console.log("connected sucessfully..")).catch((error) => console.log("error"));
+const express = require('express');
+const App = express();
+// const DB = "mongodb://127.0.0.1:27017/form";
+const DB = "mongodb+srv://root:appleapple@cluster0.riddj.mongodb.net/form?retryWrites=true&w=majority";
+
+mongoose.connect(DB).then(() => console.log("Connected To Database"))
+    .then(() => {
+        App.listen(5000);
+    })
+    .catch((err) => console.log(err));
+
+
 // mongoose.connect("6241b8c28d5e2ec0966413a2");
+
+// export default DB;
